8a9f94377a8a1c85865402b72b44501b213d0077
Aysun <aysunkapusuz@gmail.com>
2022-09-27 08:36:56 -0600

    constructor moved to the  game class from the board

--- src/index.js
+++ src/index.js
@@ -12,13 +12,7 @@ function Square(props) {
 }
   
   class Board extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        squares: Array(9).fill(null),
-        xIsNext: true,
-      };
-    }
+    
     handleClick(i) {
       const squares = this.state.squares.slice();
       if (calculateWinner(squares) || squares[i]) {
@@ -73,6 +67,15 @@ function Square(props) {
   }
   
   class Game extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        history: [{
+          squares: Array(9).fill(null),
+        }],
+        xIsNext: true,
+      };
+    }
     render() {
       return (
         <div className="game">
