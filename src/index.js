e475a25b60d8d0d66594a0d27621da0e89b8b540
Aysun <aysunkapusuz@gmail.com>
2022-09-27 07:43:34 -0600

    handleClick added to the Board class

--- src/index.js
+++ src/index.js
@@ -4,19 +4,13 @@ import './index.css';
 
 
 class Square extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
     render() {
       return (
         <button 
         className="square" 
-        onClick={() => this.setState({value: 'X'})}
+        onClick={() => this.props.onClick()}
         >
-          {this.state.value}
+          {this.props.value}
         </button>
       );
     }
@@ -29,6 +23,12 @@ class Square extends React.Component {
         squares: Array(9).fill(null),
       };
     }
+    handleClick(i) {
+      const squares = this.state.squares.slice();
+      squares[i] = 'X';
+      this.setState({squares: squares});
+    }
+    
     renderSquare(i) {
       return (
         <Square
